bd9f71f04994ef84b8305e4571b955ebbf9f4c91
SAFIRANAILA <safira.naylaaa@gmail.com>
2025-09-12 11:26:23 +0700

    second publish

---
+++ src/modul_1/DataDiri/DataDiri.java
@@ -0,0 +1,25 @@
+package modul_1.DataDiri;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class DataDiri {
+    public static void main(String[] args){
+        System.out.println("Safira Naila");
+        String nama = "Safira Naila";
+        String nim = "2024573010066";
+        LocalDate TglLahir = LocalDate.of(2006,04,1);
+        String alamat = "Punteut Meuraksa"
+        String prodi = "Teknik Informatika";
+        String jurusan ="Teknologi Informasi dan Komputer";
+        System.out.println("Nama :" + nama );
+        System.out.println("NIM :" + nim );
+        System.out.println("TglLahir :" + TglLahir);
+        System.out.println("Alamat :" + alamat);
+        System.out.println("Prodi :" + prodi);
+        System.out.println("Jurusan :" + jurusan);
+
+
+    }
+}
